3acee85ae6a1bb0d460a35c739371c294d7b2e20
Anthony <whalealice1998@gmail.com>
2024-09-25 17:53:05 +0800

    Add the function of reading a certain table from the database.

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
+import pandas as pd
 import pymysql
+import concurrent.futures
 
 
 def setting(path):
@@ -29,6 +31,7 @@ class Connector:
                 try:
                     self.db_setting = kwargs
                     self.host = kwargs['host']
+                    self.port = kwargs['port']
                     self.user = kwargs['user']
                     self.key = kwargs['key']
                     self.db = kwargs['db']
@@ -47,7 +50,7 @@ class Connector:
             1. self.conn: the connection
             2. self.cursor: the cursor
         """
-        if self.db == 'mysql':
+        if self.db_type == 'mysql':
             conn  = pymysql.connect(
                 user=self.user,
                 password=self.key,
@@ -59,3 +62,56 @@ class Connector:
         else:
             pass # TODO: Add other database connection.
         return conn, cursor
+
+class Loader:
+    """Load a table with multithreading"""
+    def __init__(self, table: str, connector: Connector, fields: str=None, chunk_size: int=1000, query: str=None):
+        # Config the table information
+        self.fields = ['all' if fields is None else fields] # Query all the fields if user do NOT indicate.
+        self.table = table
+        self.conn = connector.conn
+        self.cursor = connector.cursor
+
+        self.cursor.execute(f"""SELECT COUNT(*) FROM {table}""")
+        self.table_size = self.cursor.fetchone()[0]
+
+        self.chunk_size = chunk_size
+        self.num_chunks = self.table_size // self.chunk_size + (1 if self.table_size % self.chunk_size > 0 else 0)
+        self.query = query # user's query
+
+    def load_large_data(self):
+        all_data = pd.DataFrame()  # Store all the data from SQL
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+            futures = [] # tasks queue
+            # Commit multithreading tasks
+            for i in range(self.num_chunks):
+                offset = i * self.chunk_size
+                futures.append(executor.submit(self.read_chunked_data, offset))
+
+            # Collect the result after all the tasks are completed.
+            for future in concurrent.futures.as_completed(futures):
+                data_chunk = future.result()
+                all_data = pd.concat([all_data, data_chunk], ignore_index=True)  # Merge tasks' data.
+
+        return all_data
+
+    def read_chunked_data(self, offset):
+        # Execute embedded query either user's modified query.
+        if self.query is None:
+            if self.fields == 'all':
+                query = f"""SELECT * FROM {self.table} LIMIT {self.chunk_size} OFFSET {offset}"""
+            else:
+                fields_string = ','.join(self.fields)
+                query = f"""SELECT {fields_string} FROM {self.table} LIMIT {self.chunk_size} OFFSET {offset}"""
+        else: # In case of 'WHERE' etc. is added by user.
+            query = f"""{self.query} LIMIT {self.chunk_size} OFFSET {offset}"""
+
+        return pd.read_sql(query, self.conn)
+
+    def load_json(self, table: str):
+        pass
+
+    def parse_json(self):
+        pass
+
